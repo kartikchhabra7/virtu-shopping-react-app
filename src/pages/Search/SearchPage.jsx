3b104be898c12ea7e68f790048f9c9497d73ba58
kartik chhabra <chhabrakartik6@gmail.com>
2023-10-06 13:27:25 +0530

    🛠️  Refactor: Enhance search functionality, add loader bar, close button, and improve code readability

--- src/pages/Search/SearchPage.jsx
+++ src/pages/Search/SearchPage.jsx
@@ -6,11 +6,15 @@ import ProductCard from "../../components/features/products/ProductCard";
 import Input from "../../components/common/input/Input";
 import usePlaceholder from "../../hooks/usePlaceHolder";
 import { useNavigate } from "react-router-dom";
+import { VscChromeClose } from "react-icons/vsc";
+import useLoading from "../../hooks/useLoading";
+import LoaderContainer from "../../components/common/loaderbar/LoaderContainer";
 
 const SearchPage = () => {
   const [inputClicked, setInputClicked] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate();
+  const { hasLoad } = useLoading();
   const hotKeysInputRef = useRef(null);
   const allProducts = useProductData();
   const placeholder = usePlaceholder();
@@ -31,11 +35,7 @@ const SearchPage = () => {
         {storeProducts?.length > 0 ? (
           storeProducts
         ) : (
-          <h1
-            className="product-not-found"
-          >
-            Product not found
-          </h1>
+          <h1 className="product-not-found">Product not found</h1>
         )}
       </>
     );
@@ -57,9 +57,24 @@ const SearchPage = () => {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-light">
+      {hasLoad && <LoaderContainer />}
+      <section className="d-flex justify-content-around mt-5">
+        <div></div>
+        <div>
+          <h1 className="search-font">what can we help you find ?</h1>
+        </div>
+        <div>
+          <h4>
+            <VscChromeClose
+              onClick={() => navigate(-1)}
+              style={{ cursor: "pointer" }}
+            />
+          </h4>
+        </div>
+      </section>
+      <nav className="navbar navbar-expand-lg custom-nav mt-5">
         <div className="container-fluid">
-          <div className="d-flex justify-content-between align-items-center w-100">
+          <div className="d-flex justify-content-center align-items-center w-100">
             <div className="d-flex align-items-center">
               <Input
                 className="form-control me-2"
@@ -69,30 +84,31 @@ const SearchPage = () => {
                 ref={hotKeysInputRef}
                 onChange={(e) => setSearchQuery(e.target.value)}
                 aria-label="Search"
-                style={{ maxWidth: "950px" }}
+                style={{
+                  width: "32.438rem",
+                  height: "3.938rem",
+                  background: "#eee",
+                  border: "1px solid #484848",
+                  boxShadow: "none",
+                }}
               />
-              {!searchQuery ? (
-                <button className="btn btn-success" type="submit">
-                  Search
-                </button>
-              ) : (
-                <button
-                  className="btn btn-warning"
-                  onClick={() => setSearchQuery("")}
-                >
-                  Cancel
-                </button>
-              )}
-            </div>
-
-            <div>
-              <button className="btn btn-primary" onClick={() => navigate(-1)}>
-                Back
-              </button>
+              {/* {!searchQuery ? (
+                  <button className="btn btn-success" type="submit">
+                    Search
+                  </button>
+                ) : (
+                  <button
+                    className="btn btn-warning"
+                    onClick={() => setSearchQuery("")}
+                  >
+                    Cancel
+                  </button>
+                )} */}
             </div>
           </div>
         </div>
       </nav>
+      {/* Loader Container */}
 
       {/*  */}
       {searchQuery && (
@@ -102,6 +118,7 @@ const SearchPage = () => {
           </div>
         </div>
       )}
+
       {!inputClicked && !searchQuery && (
         <div className="search-anything">
           <ul>
@@ -128,16 +145,3 @@ const SearchPage = () => {
 };
 
 export default SearchPage;
-
-/*
-I can also directly pass this function in the Header file.
-but I passed with a helper function so that duplication will
-be avoided for multiple purposes.
-
-useHotkeys("/", (reload) => {
-    reload.preventDefault();
-    hotKeysInputRef.current.focus();
-  });
-
-
-  */
