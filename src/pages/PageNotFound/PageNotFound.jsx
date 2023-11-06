8d1f92cc969757cf120ef68ea60f85998c5ce66c
kartik chhabra <chhabrakartik6@gmail.com>
2023-11-06 16:52:05 +0530

    ✨ feat :  Utilise an error page in case the URLs don't match

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./PageNotFound.css";
+import { useNavigate } from "react-router-dom";
 
 const PageNotFound = () => {
+  const navigate = useNavigate();
   return (
-    <div>PageNotFound</div>
-  )
-}
+    <section className="pageNotFound">
+      <img
+        src="https://raw.githubusercontent.com/m-mourouh/404-Error-Page-Page-Not-Found/3d6da8f9171ab9ea25744f5019332d0bcc0accc4/Page%20Not%20found/404.svg"
+        alt="page not found"
+      />
+      <div className="wrapper">
+        <h1>Page Not Found</h1>
+        <p className="message">
+          Oops! The page you're looking for is missing..
+        </p>
+        <button className="btn" onClick={() => navigate("/")}>
+          Go To Home
+        </button>
+        <p className="copyRights"></p>
+      </div>
+    </section>
+  );
+};
 
-export default PageNotFound
+export default PageNotFound;
