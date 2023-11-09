48713c175a0c5f7a6f615f48cc1bd634c2719968
kartik chhabra <chhabrakartik6@gmail.com>
2023-11-09 17:29:07 +0530

    🚀 feat: Add loader bar for better user experience

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import Carousel from '../../components/common/carousel/Carousel'
+import React from "react";
+import CarouselContainer from "../../components/common/carousel/CarouselContainer";
+import { ThreeCircles } from "react-loader-spinner";
+import useLoading from "../../hooks/useLoading";
 
 const Home = () => {
+  const { hasLoad } = useLoading(2000);
+  if (hasLoad) {
+    return (
+      <div className="d-flex justify-content-center align-items-center min-vh-100">
+        <ThreeCircles
+          height="100"
+          width="100"
+          color="#00afef"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+          ariaLabel="three-circles-rotating"
+          outerCircleColor="#ef6500"
+          innerCircleColor="#001f3f"
+          middleCircleColor="#474973"
+        />
+      </div>
+    );
+  }
   return (
     <>
-    <Carousel/>
-    
+      <CarouselContainer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
